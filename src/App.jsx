94e7d9edf1707aef60f6dcbf2e528bc0624e2aff
obitouchiharinn <sachin2004164@gmail.com>
2025-05-28 15:05:08 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
 import { BrowserRouter } from "react-router-dom";
+import { useState, useEffect } from "react";
 
 import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, StarsCanvas } from "./components";
 
 const App = () => {
+  const [isDesktop, setIsDesktop] = useState(false);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsDesktop(window.innerWidth > 768);
+    };
+
+    handleResize(); // Set initial value
+
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
   return (
     <BrowserRouter>
       <div className='relative z-0 bg-primary'>
@@ -12,18 +26,18 @@ const App = () => {
         </div>
         <About />
         <div className='relative z-0'>
-        <Experience />
-        <StarsCanvas />
+          <Experience />
+          {isDesktop && <StarsCanvas />}
         </div>
 
         <div className='relative z-0'>
-        <Tech />
-        <StarsCanvas />
+          <Tech />
+          {isDesktop && <StarsCanvas />}
         </div>
 
         <div className='relative z-0'>
-        <Works />
-        <StarsCanvas />
+          <Works />
+          {isDesktop && <StarsCanvas />}
         </div>
         {/* <Feedbacks /> */}
         {/* <div className='relative z-0'>
